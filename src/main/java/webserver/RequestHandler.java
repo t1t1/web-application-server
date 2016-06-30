4fa01a17d9bfc572f9146648102e07d080a5f5ac
t1t1 <cha@hanmail2.net>
2016-06-30 21:43:57 +0900

    1차 완성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,16 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +29,7 @@ public class RequestHandler extends Thread {
 		
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-			response(out);
+			response(in, out);
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
@@ -32,19 +38,63 @@ public class RequestHandler extends Thread {
 	/**
 	 * @param dos
 	 */
-	private void response(OutputStream out) {
+	private void response(InputStream in, OutputStream out) {
 		DataOutputStream dos = new DataOutputStream(out);
-		byte[] body = getBody();
-		response200Header(dos, body.length);
-		responseBody(dos, body);
+		byte[] body;
+		try {
+			String fileName = getFileName(in);
+			body = getBody(fileName);
+			response200Header(dos, body.length);
+			responseBody(dos, body);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * @param in
+	 * @return
+	 */
+	private String getFileName(InputStream in) {
+		String rtnStr = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		try {
+			String line;
+			while ( (line = br.readLine()) != null) {
+				System.out.println(line);
+				if (line.indexOf("HTTP/1.1") > -1) {
+					String[] tokens = line.split(" ");
+					rtnStr = tokens[1];
+					System.out.println(rtnStr);
+				}
+				if (line.isEmpty()) {
+					break;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(rtnStr);
+		return rtnStr;
 	}
 
 	/**
 	 * @return
+	 * @throws IOException 
 	 */
-	private byte[] getBody() {
-		byte[] body = "Hello World".getBytes();
-		return body;
+	private byte[] getBody(String fileName) throws IOException {
+/*		
+		FileInputStream fis = new FileInputStream("./" + fileName);
+		InputStreamReader r = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(r);
+		while (br.readLine() != null) {
+			String line = br.readLine();
+			System.out.println(line);
+		}
+*/		
+		return Files.readAllBytes(new File("./webapp" + fileName).toPath());
 	}
 
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
